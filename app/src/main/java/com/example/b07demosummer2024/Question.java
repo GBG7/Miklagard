1bff768977263a674f91cf6d27f0881a42746d8b
h-santiago <hanszachary05@gmail.com>
2025-07-26 23:35:34 -0400

    fixed to work with Questions.java

--- app/src/main/java/com/example/b07demosummer2024/Question.java
+++ app/src/main/java/com/example/b07demosummer2024/Question.java
@@ -11,36 +11,43 @@ public class Question {
     private LinkedHashMap<String, String> optionToTip;
     private LinkedHashMap<String, Question> followup;
 
+    private String category;
+
     public String getId() {
         return id;
     }
     public void setId(String id) {
         this.id = id;
     }
+
     public String getText() {
         return text;
     }
     public void setText(String text) {
         this.text = text;
     }
+
     public String getType() {
         return type;
     }
     public void setType(String type) {
         this.type = type;
     }
+
     public String getTip() {
         return tip;
     }
     public void setTip(String tip) {
         this.tip = tip;
     }
+
     public LinkedHashMap<String, String> getOptionToTip() {
         return optionToTip;
     }
     public void setOptionToTip(LinkedHashMap<String, String> optionToTip) {
         this.optionToTip = optionToTip;
     }
+
     public LinkedHashMap<String, Question> getFollowup() {
         return followup;
     }
@@ -48,4 +55,10 @@ public class Question {
         this.followup = followup;
     }
 
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
